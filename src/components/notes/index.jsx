67980f151fc7869b2abe55658a8705fc45914f3e
caique.moraes <caique.moraes@compasso.com.br>
2022-04-12 09:16:54 -0300

    arquivos projeto

--- src/components/notes/index.jsx
+++ src/components/notes/index.jsx
@@ -1,9 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { push as Menu } from 'react-burger-menu'
-import { Column, Button } from "rbx";
+import { Column, Button } from "rbx"
 import '../../styles/notes.scss'
+import List from './list/'
+import NoteService from '../../services/notes'
 
 export default function Notes(props) {
+  const [notes, setNotes] = useState([])
+  const [current_note, setCurrentNote] = useState({ title: "", body: "", id: "" })
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const response = await NoteService.index();
+    if (response.data.length >= 1) {
+      setNotes(response.data.reverse())
+      setCurrentNote(response.data[0])
+    }
+  }
+
+  const selectNote = (id) => {
+    const note = notes.find((note) => {
+      return note._id == id;
+    })
+    setCurrentNote(note);
+  }
+
+  const createNote = async() => {
+    await NoteService.create()
+    fetchNotes()
+  }
+
   return (
     <Column.Group className="notes" id="notes">
       <Menu
@@ -20,7 +49,12 @@ export default function Notes(props) {
             Search...
           </Column>
         </Column.Group>
-        <p>List...</p>
+        <List
+          notes={notes}
+          selectNote={selectNote}
+          current_note={current_note} 
+          createNote={createNote}
+          />
       </Menu>
 
 
